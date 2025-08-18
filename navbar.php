c2afe46a8d657ae7af6e7facfd09e1d92add5c1f
siddharth23106 <siddhartharora700@gmail.com>
2025-08-18 17:47:18 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>JS Toggle Navbar</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
    <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
@@ -47,3 +47,4 @@
 
 </body>
 </html>
+
